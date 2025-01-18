46c92e83537a1232b428c5096c85155cb0195a06
Shelmith Muthoni <muthonishelmith136@gmail.com>
2025-01-18 15:42:40 +0300

    Using Fetch and API Basics Assignment

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
+let fetchedData = null; // Declare a global variable
+
+
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  //the below fetch makes a web request to the API
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then((response) => response.json()) //convert the response to JSON
+  .then((data) => {
+    console.log(data);
+    fetchedData = data; //storing the data in the global variable
+    renderBooks(data); //Pass the JSON data to the renderBooks function
+  })
+  .catch((error) => {
+    console.error("Error fetching books:", error);//Handle errors
+  })
 }
 
 function renderBooks(books) {
